b321082fb19a0f9f851ee30ad26530fc70ee82d8
jcrotzer <jcrot002@odu.edu>
2018-08-18 11:07:35 -0400

    Beginning to construct datebase connection.

--- src/com/john/times/Multiplier.java
+++ src/com/john/times/Multiplier.java
@@ -1,10 +1,23 @@
 package com.john.times;
 
-import javax.ejb.EJB;
+import java.util.List;
+
 import javax.ejb.Stateless;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 
 @Stateless
+@Entity
+@Table (name="multiplier")
 public class Multiplier {
+	@Id
+	@GeneratedValue
+	private int id;
+	private List<String> equation;
+	
 	public Multiplier()
 	{
 		
@@ -28,5 +41,15 @@ public class Multiplier {
 	public int add(int n, int m)
 	{
 		return  n + m;
+	}
+	
+	public void addToEquation(String s)
+	{
+		equation.add(s);
+	}
+	
+	public List<String> getEquation()
+	{
+		return equation;
 	}
 }
